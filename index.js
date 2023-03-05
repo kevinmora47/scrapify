6c16aa2d3b71fe0034806f8ca2a5b0d860ebb2ab
KevinMora47 <kevinmora47@gmail.com>
2023-03-05 11:30:02 -0600

    Submit form method

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-import express from 'express';
+import { Builder, By, Key, until } from 'selenium-webdriver';
+import 'chromedriver';
 
-const app = express();
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
+(async function navigateToContactUs() {
+  let driver = await new Builder().forBrowser('chrome').build();
+  try {
+    // Navigate to the page
+    await driver.get('https://totemcr.com/');
+
+    // Click on the "Contact Us" link on the navbar
+    const contactUsLink = await driver.findElement(
+      By.xpath('//a[contains(text(), "Contact Us")]')
+    );
+    await contactUsLink.click();
+
+    //Fill out the form:
+    await driver.findElement(By.id('name')).sendKeys('John Doe');
+    await driver.findElement(By.id('email')).sendKeys('john@doe.com');
+    await driver.findElement(By.id('phone')).sendKeys('+50688888888');
+    await driver.findElement(By.id('message')).sendKeys('Testing message');
+    await driver.findElement(By.id('submit_btn')).click();
+  } finally {
+    // Quit the driver
+    await driver.quit();
+  }
+})();
